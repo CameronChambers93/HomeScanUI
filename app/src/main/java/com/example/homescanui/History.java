65b14efddefcff406856de8b63b3a5219cb62568
Cameron Chambers <ctchambers@oakland.edu>
2019-03-30 11:39:42 -0400

    update to include dynamo db read and write

--- app/src/main/java/com/example/homescanui/History.java
+++ app/src/main/java/com/example/homescanui/History.java
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+
+import java.time.LocalTime;
 
 public class History extends AppCompatActivity {
 
@@ -21,7 +24,13 @@ public class History extends AppCompatActivity {
 
         // AWSMobileClient enables AWS user credentials to access your table
         AWSMobileClient.getInstance().initialize(this).execute();
-        AWSCredentialsProvider credentialsProvider = AWSMobileClient.getInstance().getCredentialsProvider();
+
+        //This credentialsProvider allows users to access the DynamoDB
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-west-2:5f5ebaab-898a-4a97-a21b-fa40128de072", // Identity pool ID
+                Regions.US_WEST_2 // Region
+        );
         AWSConfiguration configuration = AWSMobileClient.getInstance().getConfiguration();
 
         // Add code to instantiate a AmazonDynamoDBClient
@@ -30,13 +39,38 @@ public class History extends AppCompatActivity {
                 .dynamoDBClient(dynamoDBClient)
                 .awsConfiguration(configuration)
                 .build();
-        createNews();
+        createHistory();
+        try {
+            wait(3000);
+        }
+        catch (Exception e){
+            Log.e("Error", "idk");
+        }
+        readHistory();
     }
 
-    public void createNews() {
-        final UsersDO userItem= new UsersDO();
+    public void readUser() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("Go: ", "Going");
+                try {
+                    UsersDO newsItem = dynamoDBMapper.load(
+                            UsersDO.class,
+                            "Article1",
+                            "This is the article content");
+                    // Item read
+                    Log.d("SUCCESS, ITEM GENERATED: ", "\n"+newsItem.getPiId()+"\n"+newsItem.getUserId());
+                }
+                catch(Exception e){
+                    Log.e("FAILURE: ", e.toString());
+                }
+            }
+        }).start();
+    }
 
-        userItem.setUserId("User1");
+    public void createUser() {
+        final UsersDO userItem= new UsersDO();
 
         userItem.setUserId("Article1");
         userItem.setPiId("This is the article content");
@@ -45,6 +79,45 @@ public class History extends AppCompatActivity {
             @Override
             public void run() {
                 dynamoDBMapper.save(userItem);
+                Log.e("Test", "Success");
+                // Item saved
+            }
+        }).start();
+    }
+
+
+    public void readHistory() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("Go: ", "Going");
+                try {
+                    HisroryDO historyItem = dynamoDBMapper.load(
+                            HisroryDO.class,
+                            "User1",
+                            "Current time");
+
+                    // Item read
+                    Log.d("SUCCESS, ITEM GENERATED: ", "\n"+historyItem.getUserId()+"\n"+historyItem.getTime()+"\n"+historyItem.getLockAction());
+                }
+                catch(Exception e){
+                    Log.e("FAILURE: ", e.toString());
+                }
+            }
+        }).start();
+    }
+
+    public void createHistory() {
+        final HisroryDO historyItem = new HisroryDO();
+
+        historyItem.setUserId("User1");
+        historyItem.setTime("Current time");
+        historyItem.setLockAction("Locked");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                dynamoDBMapper.save(historyItem);
                 Log.e("Test", "Success");
                 // Item saved
             }
